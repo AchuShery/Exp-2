4fd9c9c626f43f0951a71cc8bf2045d5703be3aa
AchuShery <162905317+AchuShery@users.noreply.github.com>
2024-03-14 22:08:06 +0530

    Exp2.java

--- Exp2.java
+++ Exp2.java
@@ -1,8 +1,9 @@
 public class Exp2 {
   public static void main(String[] args) {
-  System.Out.println("Hello Everyone");
+  System.Out.println("Hello World");
   System.Out.println("I am Achu Shery");
-    System.Out.println("Hello World");
+    int b = 7 ; 
+    System.Out.println(b);
   }
 }
   
